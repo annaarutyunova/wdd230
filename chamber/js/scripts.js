c07c1ad63cd0fcf633db29beba2bcfd3abdc4097
anna.arutyunova <anna.arutyunova8848@gmail.com>
2022-10-16 19:21:49 -0600

    commit

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,10 +1,12 @@
 let date = new Date();
+let dateint = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
 let currentDate = document.lastModified;
 let year = date.getFullYear();
 document.querySelector('.modified').textContent = currentDate;
 document.querySelector('.year').textContent = year;
 console.log(currentDate);
 console.log(year);
+document.querySelector("#datep").textContent = dateint;
 
 function toggleMenu(){
     document.getElementById('primaryNav').classList.toggle('open');
