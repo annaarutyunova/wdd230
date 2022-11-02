c3c5d3936cddf90e2ed7826fc902b4575ff013c9
anna.arutyunova <anna.arutyunova8848@gmail.com>
2022-11-02 17:21:49 -0600

    add media query

--- chamber_copy/js/windchill.js
+++ chamber_copy/js/windchill.js
@@ -4,7 +4,7 @@ let windChill = document.getElementById("wc").textContent;
 
 if(temperature <= 10 && windSpeed >= 4.8){
     let chill = Math.round((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temperature*Math.pow(windSpeed,0.16)));
-    windChill = chill;
+    windChill = concat({chill}, "km/h");
     console.log(chill);
     console.log(windChill);
 }
