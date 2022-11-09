453f684255fff0c1538ba5bf09febf6764aa7ca7
anna.arutyunova <anna.arutyunova8848@gmail.com>
2022-11-09 11:17:20 -0700

    add join page

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,11 +6,19 @@ function displayProphets(item){
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let img = document.createElement('img');
+    let dob = document.createElement('p');
+    let pob = document.createElement('img');
 
     h2.textContent = item.name + ' ' + item.lastname;
+    dob.textContent = item.birthdate;
+    dob.textContent = item.birthplace;
     img.setAttribute('src', item.imageurl);
+    img.setAttribute('alt', item.name + ' ' + item.lastname);
 
     card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(pob);
+    card.appendChild(img);
     document.querySelector('.cards').appendChild(card);
 }
 
