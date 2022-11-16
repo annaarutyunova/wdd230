8893a2893ebb7ab3073008bf557586fde5ab4b72
anna.arutyunova <anna.arutyunova8848@gmail.com>
2022-11-16 12:07:44 -0700

    add stuff

--- lesson10/weather-api.js
+++ lesson10/weather-api.js
@@ -7,5 +7,12 @@ const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
     console.log(jsObject);
+    document.querySelector('#current-temp').textContent = Math.round((jsObject.main.temp - 273.15)* 1.8000 + 32.00);
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
   };
 getWeather();
