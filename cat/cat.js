4a4746a499b7623cce52e9b66cc4550fafa88b9e
annaarutyunova <anna.arutyunova8848@gmail.com>
2022-12-16 10:27:28 -0700

    add content

--- cat/cat.js
+++ cat/cat.js
@@ -1,1 +1,18 @@
-const requestURL = "https://annaarutyunova.github.io/wdd230/chamber_copy/data.json";
+const requestURL = "https://annaarutyunova.github.io/wdd230/cat/cat.json";
+const request = "https://raw.githubusercontent.com/annaarutyunova/wdd230/main/cat/cat.json";
+let cats = [];
+
+
+async function getCats(requestURL){
+    const response = await fetch(requestURL);
+    console.log(response);
+    if(response.ok){
+        const jsObject = await response.json();
+        console.log(jsObject);
+        cats = jsObject["cats"];
+        console.log(cats[0].breed);
+    }
+}
+
+
+getCats(requestURL);
