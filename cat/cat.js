f609dacd170c733fa5532ef357df3b49cbf890cb
annaarutyunova <anna.arutyunova8848@gmail.com>
2022-12-16 17:56:13 -0700

    add contnt

--- cat/cat.js
+++ cat/cat.js
@@ -11,8 +11,87 @@ async function getCats(requestURL){
         console.log(jsObject);
         cats = jsObject["cats"];
         console.log(cats[0].breed);
+        cats.forEach(displayCats);
     }
 }
 
+function displayCats(item){
+    let li = document.createElement("li");
+    let image = document.createElement("img");
+    image.setAttribute('data-src', item.image)
+    image.setAttribute('src', "https://dummyimage.com/500x300/ccc/fff")
+    image.setAttribute('alt', item.breed)
+    li.appendChild(image);
+    document.getElementById("viewcatsul").appendChild(li)
+    preloadingImage(image)
+    const options = {
+        threshold: 1,
+        rootMargin:"0px 0px 100px 0px"
+    }
+
+    const io = new IntersectionObserver (
+        (entries, io) => {
+            entries.forEach(entry => {
+                if(!entry.isIntersecting){
+                    return;
+                }
+                else{
+                    preloadingImage(entry.target);
+                    io.unobserve(entry.target) // target tells us which image we are on
+                }
+            })
+        },
+        {
+            options
+        }
+    );
+    
+    placeholder.forEach(item => {
+        io.observe(item);
+    })
+
+}
 
 getCats(requestURL);
+
+
+
+// Attempt to lazy load
+
+const image = document.querySelectorAll('img');
+const options = {
+    threshold: 1,
+    rootMargin:"0px 0px 100px 0px"
+}
+
+const placeholder = document.querySelectorAll('[data-src]');
+
+function preloadingImage(img){
+    const source = img.getAttribute('data-src');
+    if(!source){
+        return;
+    }
+
+    img.src = source;
+    }
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }
+            else{
+                preloadingImage(entry.target);
+                io.unobserve(entry.target) // target tells us which image we are on
+            }
+        })
+    },
+    {
+        options
+    }
+);
+
+placeholder.forEach(item => {
+    io.observe(item);
+})
--- chamber_copy/js/discover.js
+++ chamber_copy/js/discover.js
@@ -1,34 +1,3 @@
-// const images = document.querySelectorAll("[data-src]");
-
-// function preloadImage(img) {
-//     const src = img.getAttribute("data-src");
-//     if(!src) {
-//         return;
-//     }
-
-//     img.src = src;
-// }
-
-// const imgOptions = {
-//     threshold: 1,
-//     rootMargin: "0px 0px -500px 0px"
-// }
-
-// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) {
-//             return;
-//         } else {
-//             preloadingImage(entry.target);
-//             imgObserver.unobserve(entry.target);
-//         }
-//     })
-// }, imgOptions);
-
-// images.forEach(image => {
-//     imgObserver.observe(image);
-// });
-
 const image = document.querySelectorAll('img');
 const options = {
     threshold: 1,
